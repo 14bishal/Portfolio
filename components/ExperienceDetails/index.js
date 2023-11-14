98412940bd6e813489738950ed9584a53e13b59e
Bishal1184 <bishal.saha@cogoport.com>
2023-11-14 13:49:00 +0530

    add details page

--- components/ExperienceDetails/index.js
+++ components/ExperienceDetails/index.js
@@ -1,13 +1,31 @@
-import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
+import ProjectDetails from './ProjectDetails';
+import EXPERIENCE_PROJECT from '../../constants/experience-project';
 
 function ExperienceDetails() {
     return (
         <SafeAreaView>
             <ScrollView>
-                <View>
-                    <Text>ExperienceDetails</Text>
-                </View>
+                {(EXPERIENCE_PROJECT || []).map(item => {
+                    const {
+                        label = '',
+                        name = '',
+                        skills = [],
+                        description = [],
+                        imageUrl,
+                    } = item || {};
+
+                    return (
+                        <ProjectDetails
+                            key={name}
+                            title={label}
+                            skills={skills}
+                            description={description}
+                            imageUrl={imageUrl}
+                        />
+                    );
+                })}
             </ScrollView>
         </SafeAreaView>
     );
---
+++ constants/experience-project.js
@@ -0,0 +1,59 @@
+const EXPERIENCE_PROJECT = [
+    {
+        label: 'Call Routing Platform',
+        name: 'call_routing_platform',
+        description: [
+            {
+                label: 'Developed a feature-rich Call Routing Platform, allowing efficient communication between agents and customers.',
+                key: 'call_routing_1',
+            },
+            {
+                label: 'Integrated call facility and tracking mechanisms to monitor call duration and performance metrics.',
+                key: 'call_routing_2',
+            },
+        ],
+        skills: ['ReactJs', 'Redux', 'nivoChart'],
+        imageUrl:
+            'https://cdn.cogoport.io/cms-prod/cogo_admin/vault/original/telephone.jpg',
+    },
+    {
+        label: 'Build a mini e-market space Cogostore',
+        name: 'mini_market_cogostore',
+        description: [
+            {
+                label: 'Architected a Voucher System to manage voucher generation, distribution, and redemption.',
+                key: 'mini_market_cogostore_1',
+            },
+            {
+                label: 'Implemented a responsive website of cogostore for customer use and add functionality like add or delete the product from cart, add address, etc.',
+                key: 'mini_market_cogostore_2',
+            },
+        ],
+        skills: ['ReactJs', 'Redux', 'useForm'],
+        imageUrl:
+            'https://imagedelivery.net/xE-VtsYZUS2Y8MtLMcbXAg/4e19b54c784a01db5da1/sm',
+    },
+    {
+        label: 'Push Notifications',
+        name: 'push_notifications',
+        description: [
+            {
+                label: 'Implemented push notification feature using the new Notification API in JavaScript',
+                name: 'push_notifications_1',
+            },
+            {
+                label: 'Customized notification content and behavior based on user preferences',
+                name: 'push_notifications_2',
+            },
+            {
+                label: 'Integrated push notification functionality seamlessly into the web application',
+                name: 'push_notifications_3',
+            },
+        ],
+        skills: ['Javascript', 'ReactJs'],
+        imageUrl:
+            'https://imagedelivery.net/xE-VtsYZUS2Y8MtLMcbXAg/467db981cf2f98ca15af/sm',
+    },
+];
+
+export default EXPERIENCE_PROJECT;
